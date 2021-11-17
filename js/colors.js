893166e9a3b1976c6f1d2181af3d297352508c2d
Opilane TTHK <opilane@tthk.ee>
2021-11-17 15:01:43 +0200

    Updated Colors

--- js/colors.js
+++ js/colors.js
@@ -3,11 +3,22 @@ const myButton = document.querySelector('button');
 const myBox = document.querySelector('.box');
 const colors = ['red', 'green', 'blue', 'yellow'];
 // console.log(myButton);
+let randomValues = [];
+
+let name = `HarryPotter`;
+
+console.log(`Hello ${name}`)
 
 myButton.addEventListener('click', ChangeColor);
 
 function ChangeColor(){
-    let rnd = math.floor(math.random() * colors.length);
-    console.log(rnd);
-    myBox.style.backgroundColor = colors[rnd];
+    if(randomValues.length === 0) {
+        for(let i = 0; i < colors; i++){
+            randomValues.push(i);
+            console.log(`${i} added`);
+        }
+    }
+    let RandomIndex = math.floor(math.random() * randomValues.length);
+    let indexFromMyarray = 
+    myBox.style.backgroundColor = colors[indexFromMyarray];
 }
